7a75c0b53e10b8e95da74babf4ac235b2bcad470
Abhijit Parad <94881938+ErzaXAbhi@users.noreply.github.com>
2023-05-06 00:45:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.phone-icon').addEventListener('click', function() {
+    window.location.href = 'tel:+91 9637468192';
+  });
+
+  document.querySelector('.message-icon').addEventListener('click', function() {
+    window.open('https://example.com/chat', 'Chat Window', 'width=400,height=600');
+  });
+  
+  
